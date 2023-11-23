dcfdb45e7856a3aea1106ac1f9d7c49e18b63425
awf952 <144993468+awf952@users.noreply.github.com>
2023-11-23 22:34:55 +0700

    Add files via upload

---
+++ Tugas p16.js
@@ -0,0 +1,11 @@
+// Contoh kode JavaScript
+
+// Variabel
+let nama = 'Wahid';
+let usia = 22;
+let isMenikah = true;
+
+// Menampilkan output
+console.log('Nama:', nama);
+console.log('Usia:', usia);
+console.log('Status Menikah:', isMenikah);
